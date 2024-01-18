eeed087aaedbb26ee00ad6a406eafeb67e7ba81e
fercamposdev <fernando.developments@gmail.com>
2024-01-18 14:36:41 -0300

    feat: lang with flags and responsive

--- js/i18n.js
+++ js/i18n.js
@@ -1,41 +1,3 @@
-/* function updateContent() {
-  const elements = document.getElementsByClassName("i18n");
-  for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    const key = element.getAttribute("data-i18n");
-    element.innerHTML = i18next.t(key);
-  }
-}
-
-async function i18Loader() {
-  const langs = ["en", "es"];
-  const jsons = await Promise.all(
-    langs.map((lang) => fetch("lang/" + lang + ".json").then((r) => r.json()))
-  );
-
-  const res = langs.reduce((acc, lang, idx) => {
-    acc[lang] = { translation: jsons[idx] };
-    return acc;
-  }, {});
-
-  await i18next.init({
-    lng: "en",
-    debug: true,
-    resources: res
-  });
-  updateContent();
-  i18next.on("languageChanged", () => {
-    updateContent();
-  });
-  const langSelector = document.getElementById("langSelector");
-  langSelector.removeAttribute("disabled");
-  langSelector.addEventListener("change", (e) => {
-    i18next.changeLanguage(e.target.value);
-  });
-}
-
-i18Loader(); */
-
 function updateContent() {
   const elements = document.getElementsByClassName("i18n");
   for (let i = 0; i < elements.length; i++) {
@@ -58,26 +20,28 @@ async function i18Loader() {
 
   await i18next.init({
     lng: "en",
-    debug: true,
+    // debug: true,
     resources: res
   });
   updateContent();
-  i18next.on("languageChanged", (newLang) => {
-    langSelector.innerText = newLang === 'es' ? 'Español' : 'English';
+
+  i18next.on("languageChanged", (lang) => {
+    langFlag.setAttribute('src', `/img/flags/${lang}.png`)
     updateContent();
   });
 
-  const langSelector = document.getElementById("langSelector");
-  const btnEn = document.getElementById("langEn");
-  const btnEs = document.getElementById("langEs");
+  const langSelector = document.getElementById("lang-selector");
+  const langFlag = document.getElementById("lang-flag");
+  const btnEn = document.getElementById("lang-en");
+  const btnEs = document.getElementById("lang-es");
 
   langSelector.removeAttribute("disabled");
-  btnEn.addEventListener("click", (e) => {
-    i18next.changeLanguage(e.target.value);
+  btnEn.addEventListener("click", () => {
+    i18next.changeLanguage('en');
   });
 
-  btnEs.addEventListener("click", (e) => {
-    i18next.changeLanguage(e.target.value);
+  btnEs.addEventListener("click", () => {
+    i18next.changeLanguage('es');
   });
 }
 
