03d119a12e81dd11a2c692554d443313a5395a0a
fercamposdev <fernando.developments@gmail.com>
2024-05-23 23:43:58 -0300

    feat: privacy policies

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,20 @@
 import { Lang } from '@/types/lang';
 
 import es from './es.json';
+import esPolicies from './es.policies.json';
+
 import en from './en.json';
+import enPolicies from './en.policies.json';
 
 export const LANGS: Lang[] = ['es', 'en'];
 
 export const dictionary = {
-  es,
-  en,
+  es: {
+    ...es,
+    policies: { ...esPolicies },
+  },
+  en: {
+    ...en,
+    policies: { ...enPolicies },
+  },
 }
