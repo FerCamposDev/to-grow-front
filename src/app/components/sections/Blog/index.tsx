98f5f0fc098bfeb6fdcc45cbd4d84644b642b257
fercamposdev <fernando.developments@gmail.com>
2024-05-29 00:20:28 -0300

    feat: blog section with last posts

--- src/app/components/sections/Blog/index.tsx
+++ src/app/components/sections/Blog/index.tsx
@@ -4,29 +4,34 @@ import SectionTitle from "../../shared/SectionTitle"
 import PostCard from "./PostCard";
 import { getPosts } from "@/helpers/posts";
 import { LangProps } from "@/types/lang";
+import { dictionary } from "@/i18n";
+import Link from "next/link";
 
 const BlogSection = ({ lang }: LangProps) => {
   const posts = use(getPosts(lang));
+  const wordings = dictionary[lang].blog_section;
 
   return (
     <div id="blog" className="py-32">
       <div className="container px-4 mx-auto xl:px-48">
         <div className="flex flex-col gap-4">
           <SectionTitle
-            title="Our Blog"
-            description="Latest Blog & News"
+            title={wordings.header}
+            description={wordings.title}
           />
 
-          <div className="flex justify-center gap-4">
+          <div className="flex justify-center gap-4 flex-wrap">
             {posts.map((post) => (
               <PostCard key={post.slug} data={post} lang={lang} />
             ))}
           </div>
 
           <div className="flex justify-center">
-            <Button>
-              View all Posts
-            </Button>
+            <Link href={`/${lang}/blog`}>
+              <Button>
+                {wordings.view_all}
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
